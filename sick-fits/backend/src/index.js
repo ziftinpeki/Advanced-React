851b140141cc57bb761c83a728a00028172e5233
Mehmet HAZMAN <mehmet.hazman@gmail.com>
2020-03-16 13:51:24 +0300

    Module 3 - updates

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,21 @@
 // let's go!
+require("dotenv").config();
+
+const createServer = require("./createServer");
+const db = require("./createServer");
+
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(
+      `Server is now running on port http://localhost:${deets.port} `
+    );
+  }
+);
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO : check if they are logged in
+    const Item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-module.exports = mutations;
+    return Item;
+  }
+};
+
+module.exports = Mutations;
